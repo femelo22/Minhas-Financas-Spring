e2a4b327c8b8c592a25d2f162b3a7f7140c91172
luiz fernando <luiz123jfmg@gmail.com>
2021-05-16 20:10:52 -0300

    Criando resources

---
+++ src/main/java/br/com/luiz/resources/UsuarioResource.java
@@ -0,0 +1,12 @@
+package br.com.luiz.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UsuarioResource {
+	
+	
+
+}
